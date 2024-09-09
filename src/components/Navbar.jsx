930e50aa75351ee83db36d87baa26f58782f9249
Abdallahosama55 <abdallahosama8053@gmail.com>
2024-09-09 20:25:37 +0300

    v1 conect host

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -60,9 +60,8 @@ function Navbar() {
             id="navbar-cta"
           >
             {isMenuOpen ? (
-              <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-              <MenuItem href="#">الرئيسية</MenuItem>
-              <HashLink to="#aboutus" scroll={smoothScroll}>
+              <ul className="flex flex-col gap-5 font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+      <HashLink to="#aboutus" scroll={smoothScroll}>
                 من نحن
               </HashLink>
               <HashLink to="#pricePlan" scroll={smoothScroll}>
