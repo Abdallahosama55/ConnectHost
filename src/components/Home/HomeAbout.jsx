04528ce2f784a97c3eaa4c398e06cca632dace9f
Abdallahosama55 <abdallahosama8053@gmail.com>
2024-09-09 17:41:27 +0300

    v1 conect host

--- src/components/Home/HomeAbout.jsx
+++ src/components/Home/HomeAbout.jsx
@@ -4,17 +4,17 @@ import Logo from "../../assets/Logo.png";
 function HomeAbout() {
   return (
     <div
-      className="lg:mx-16 mx-5 grid lg:grid-cols-2 grid-cols-1 lg:gap-24 gap-8 justify-center items-center font-cairo"
+      className="lg:mx-16 mx-5 grid lg:grid-cols-2 grid-cols-1 lg:gap-24 gap-8 justify-center items-center font-cairo "
       dir="rtl"
     >
-      <div className=" w-full h-full py-12">
+      <div className=" w-full h-full py-12  lg:order-1 order-2">
         <img
           src={illustration}
           alt="illustration"
           className=" lg:w-[580px] lg:h-[500px] "
         />
       </div>
-      <div className=" w-full h-full items-center justify-center flex flex-col gap-5">
+      <div className=" w-full h-full items-center justify-center flex flex-col gap-5  lg:order-2 order-1">
         <img src={Logo} alt="illustration" className="w-[424px] h-[198px]" />
         <p className=" text-black mx-auto leading-10 text-lg">
           هي منصة متكاملة لإدارة حجوزات الوحدات السكنية المدرجة على منصات مثل
